ab1ad77ae2ef4be4cd7a9793796dc59667f573fc
Gregg Johnson <gregg.johnson@hotmail.co.uk>
2026-09-14 15:42:47 +0100

    Fix output resource group reference to rg

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,29 @@
-# Output variable definitions
 output "resource_group_id" {
-  description = "resource group id"
-  value       = azurerm_resource_group.resource_group.id 
+  description = "The ID of the Resource Group"
+  value       = azurerm_resource_group.rg.id
 }
+
 output "resource_group_name" {
-  description = "The name of the resource group"
-  value       = azurerm_resource_group.resource_group.name
+  description = "The Name of the Resource Group"
+  value       = azurerm_resource_group.rg.name
 }
+
 output "resource_group_location" {
-  description = "resource group location"
-  value       = azurerm_resource_group.resource_group.location
+  description = "The Location of the Resource Group"
+  value       = azurerm_resource_group.rg.location
 }
+
 output "storage_account_id" {
-  description = "storage account id"
+  description = "The ID of the Storage Account"
   value       = azurerm_storage_account.storage_account.id
 }
+
 output "storage_account_name" {
-  description = "storage account name"
-  value       = azurerm_storage_account.storage_account.name 
+  description = "The Name of the Storage Account"
+  value       = azurerm_storage_account.storage_account.name
 }
 
-
+output "storage_account_web_endpoint" {
+  description = "The primary web endpoint for the static website"
+  value       = azurerm_storage_account.storage_account.primary_web_endpoint
+}
