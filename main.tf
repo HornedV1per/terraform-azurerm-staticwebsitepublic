59662392da3dfeafb7c8e3da4f506f2c3e050e5a
Gregg Johnson <gregg.johnson@hotmail.co.uk>
2026-09-14 15:13:09 +0100

    Fix static_website resource and deprecated numeric attribute for AzureRM v4+

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "random_string" "myrandom" {
   length = 6
   upper = false 
   special = false
-  number = false   
+  numeric = false   
 }
 
 # Create Resource Group
@@ -17,19 +17,20 @@ resource "azurerm_resource_group" "resource_group" {
   location = var.location
 }
 
-# Create Azure Storage account
+# Azure Storage Account Resource
 resource "azurerm_storage_account" "storage_account" {
-  name                = "${var.storage_account_name}${random_string.myrandom.id}"
-  resource_group_name = azurerm_resource_group.resource_group.name
- 
+  name                     = "${var.storage_account_name}${random_string.myrandom.id}"
+  resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = var.storage_account_tier
   account_replication_type = var.storage_account_replication_type
   account_kind             = var.storage_account_kind
- 
-  static_website {
-    index_document = var.static_website_index_document
-    error_404_document = var.static_website_error_404_document  
-  }
+}
+
+# Separate Static Website Configuration Resource (AzureRM v4+)
+resource "azurerm_storage_account_static_website" "static_website" {
+  storage_account_id = azurerm_storage_account.storage_account.id
+  index_document     = var.static_website_index_document
+  error_404_document = var.static_website_error_404_document
 }
 
