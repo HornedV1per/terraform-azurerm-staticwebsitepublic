388066e8cfd6db6ba9502663f16b13f01d5fe553
Gregg Johnson <gregg.johnson@hotmail.co.uk>
2026-09-14 15:35:11 +0100

    Remove provider block and duplicate storage account declaration

--- main.tf
+++ main.tf
@@ -1,38 +1,22 @@
-# Provider Block
-provider "azurerm" {
- features {}          
-}
-
 # Random String Resource
 resource "random_string" "myrandom" {
-  length = 6
-  upper = false 
+  length  = 6
+  upper   = false
   special = false
-  numeric = false   
+  numeric = false
 }
 
-# Create Resource Group
 # Create Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-# Azure Storage Account
+# Azure Storage Account (Single Declaration)
 resource "azurerm_storage_account" "storage_account" {
   name                     = "${var.storage_account_name}${random_string.myrandom.id}"
-  resource_group_name      = azurerm_resource_group.rg.name # Implicit dependency
+  resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
-  account_tier             = var.storage_account_tier
-  account_replication_type = var.storage_account_replication_type
-  account_kind             = var.storage_account_kind
-}
-
-# Azure Storage Account Resource
-resource "azurerm_storage_account" "storage_account" {
-  name                     = "${var.storage_account_name}${random_string.myrandom.id}"
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
   account_tier             = var.storage_account_tier
   account_replication_type = var.storage_account_replication_type
   account_kind             = var.storage_account_kind
@@ -44,4 +28,3 @@ resource "azurerm_storage_account_static_website" "static_website" {
   index_document     = var.static_website_index_document
   error_404_document = var.static_website_error_404_document
 }
-
